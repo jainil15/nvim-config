111ccaadd45a92346a5614a81dcb25a145da35cb
Jainil <jainilpatel115@gmail.com>
2024-08-13 22:57:39 +0530

    Add

---
+++ init.lua
@@ -0,0 +1,5 @@
+require(   "jainil" )
+require'lspconfig'.biome.setup{}
+
+
+
---
+++ lua/jainil/init.lua
@@ -0,0 +1,12 @@
+require("jainil.remap")
+require("jainil.packer")
+require("jainil.set")
+require("jainil.null-ls")
+
+
+
+
+
+
+
+
