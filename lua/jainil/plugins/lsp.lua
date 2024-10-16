07de667f66b112e77110d3340197433bb6ead48c
jainil15 <jainilpatel115@gmail.com>
2024-10-17 00:15:08 +0530

    Fix: BUG WITH THE NVIM_CMP AUTOCOFTHNGOIAEHJT

--- lua/jainil/plugins/lsp.lua
+++ lua/jainil/plugins/lsp.lua
@@ -81,10 +81,14 @@ return {
 					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 				end,
 			},
+      preselect = cmp.PreselectMode.None,
+      completion = {
+        completeopt = "menu,menuone,noselect"
+      },
 			mapping = cmp.mapping.preset.insert({
 				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<C-<CR>>"] = cmp.mapping.confirm({ select = false }),
 				["<C-Space>"] = cmp.mapping.complete(),
 			}),
 			sources = cmp.config.sources({
